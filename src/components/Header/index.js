13187131015577455cd34e6fe8686b010fb55891
Marcos Ribeiro <131191211+Marcsfic98@users.noreply.github.com>
2025-05-17 00:19:27 -0300

    seçoes info perfil e usuarios finalizadas

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import './style.css';
+
+
+const Header = () => {
+    return (
+      <header>
+        <h1>Git Find</h1>
+      </header>
+    );
+  }
+  
+  export {Header};
+  
--- src/pages/home/App.js
+++ src/pages/home/App.js
@@ -1,8 +1,35 @@
+import {Header} from "../../components/Header"
+
+import './style.css'
 
 function App() {
   return (
     <div className="App">
-      ola
+      <Header/>
+      <div className="conteudo">
+        <img src="" className="background" alt="background app"/>
+        <div className="info">
+
+            <div>
+                <input name="usuario" placeholder="@Username"></input>
+                <button>Buscar</button>
+            </div> 
+
+            <div className="perfil">
+                <img src="https://avatars.githubusercontent.com/u/131191211?v=4" className="profile" alt="foto-usuario"/>
+                <div>
+                  <h3>Marcos Ribeiro</h3>
+                  <span>@marcxs_s</span>
+                  <p>Descriçao</p>
+                </div>
+
+            </div>{/*perfil*/}
+
+            
+            <hr/>
+
+        </div>{/*info*/}
+      </div>{/* conteudo */}
     </div>
   );
 }
