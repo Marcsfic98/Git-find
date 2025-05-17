be2807ae702c3d7008736673c7eb48f111db901c
Marcos Ribeiro <131191211+Marcsfic98@users.noreply.github.com>
2025-05-17 11:22:08 -0300

    estilizaçao completa

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,10 @@
 import './style.css';
-
+import pngegg from '../../assents/logo/pngegg.png'
 
 const Header = () => {
     return (
       <header>
+        <img src={pngegg} alt='logo'/>
         <h1>Git Find</h1>
       </header>
     );
--- src/pages/home/App.js
+++ src/pages/home/App.js
@@ -1,9 +1,34 @@
+import { useState } from "react";
 import {Header} from "../../components/Header"
 
 import { ItenList } from "../../components/List";
 import './style.css'
 
 function App() {
+
+    const [user, setUser] = useState('');
+    const [currentUser, setCurrentUser] = useState(null);
+    const [repos, setRepos] = useState(null);
+
+    const handleGetData = async () => {
+      const useData = await fetch(`https://api.github.com/users/${user}`);
+      const newUser = await useData.json();
+      
+      if(newUser.name){
+        const {avatar_url,name,bio,login} = newUser
+        setCurrentUser({avatar_url,name,bio,login})
+
+        const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
+        const newRepos = await reposData.json();
+
+        if(newRepos.length){
+          setRepos(newRepos);
+        }
+      }
+    }
+
+
+
   return (
     <div className="App">
       <Header/>
@@ -12,29 +37,40 @@ function App() {
         <div className="info">
 
             <div>
-                <input name="usuario" placeholder="@Username"></input>
-                <button>Buscar</button>
+                <input name="usuario" value={user} onChange={event => setUser(event.target.value)} placeholder="@Username"></input>
+                <button onClick={handleGetData}>Buscar</button>
             </div> 
 
+            {currentUser?.name ?(
+              
+              <>
             <div className="perfil">
-                <img src="https://avatars.githubusercontent.com/u/131191211?v=4" className="profile" alt="foto-usuario"/>
-                <div>
-                  <h3>Marcos Ribeiro</h3>
-                  <span>@marcxs_s</span>
-                  <p>Descriçao</p>
-                </div>
+              <img src={currentUser.avatar_url} className="profile" alt="foto-usuario"/>
+              <div>
+                <h3>{currentUser.name} </h3>
+                <span>{currentUser.login} </span>
+                <p>{currentUser.bio} </p>
+              </div>
 
-            </div>{/*perfil*/}
-
-            
-            <hr/>
-
-            <div className="repositorio">
-              <h4>Repositórios</h4>
-              <ItenList title='teste' description='desc teste'/>
-              <ItenList title='teste' description='desc teste'/>
-              <ItenList title='teste' description='desc teste'/>
             </div>
+
+              <hr/> 
+              </>
+            ) : null }
+            
+           
+            {repos?.length ?(
+
+              <div className="repositorio">
+                <h4>Repositórios</h4>
+
+                {repos.map(repo => (
+                  <ItenList title={repo.name} description={repo.description}/>
+                ))}
+                
+              </div>
+            ):null}
+
 
         </div>{/*info*/}
       </div>{/* conteudo */}
